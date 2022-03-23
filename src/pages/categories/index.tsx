e9cc719bbdbeee796b57ddfcf5642a8ed94dad86
Arli Machado <arli.machado@gmail.com>
2022-03-22 21:30:31 -0300

    Listando categorias a partir do backend

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,33 +1,27 @@
 import ContentHeader from 'components/ContentHeader';
 import ItemCard from 'components/ItemCard';
 import Layout from 'components/Layout';
+import { GetServerSideProps } from 'next';
 import { useEffect, useState } from 'react';
-import categories from 'repositories/categories';
+import { setupAPIClient } from 'services/api';
+import { withSSRAuth } from 'utils/withSSRAuth';
 import * as S from './styles';
 
 interface ICategoryProps {
   id: number;
-  name: string;
-  status: string;
+  description: string;
+  is_active: boolean;
+  status?: string;
   // isActive: boolean;
-  tagColor: string;
+  // tagColor: string;
 }
 
-export default function Categories() {
+interface ICategoriesProps {
+  categories: ICategoryProps[];
+}
+
+export default function Categories({ categories }: ICategoriesProps) {
   const [data, setData] = useState<ICategoryProps[]>([]);
-
-  useEffect(() => {
-    const formattedData = categories.map(category => {
-      return {
-        id: Math.random() * categories.length,
-        name: category.name,
-        status: category.isActive ? 'Ativo' : 'Inativo',
-        tagColor: category.tagColor,
-      };
-    });
-
-    setData(formattedData);
-  }, []);
 
   return (
     <Layout>
@@ -35,12 +29,12 @@ export default function Categories() {
         <ContentHeader title="Categorias" lineColor="#f7931B"></ContentHeader>
 
         <S.Content>
-          {data.map(category => (
+          {categories.map(category => (
             <ItemCard
               key={category.id}
-              title={category.name}
-              subtitle={category.status}
-              tagColor={category.tagColor}
+              title={category.description}
+              subtitle="teste"
+              tagColor="#f7931B"
             />
           ))}
         </S.Content>
@@ -48,3 +42,26 @@ export default function Categories() {
     </Layout>
   );
 }
+
+export const getServerSideProps = withSSRAuth(async ctx => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/categories');
+
+  const categories: ICategoryProps[] = response.data;
+
+  const formattedData = categories.map(({ id, description, is_active }) => {
+    return {
+      id,
+      description,
+      is_active,
+      status: is_active ? 'Ativo' : 'Inativo',
+      tagColor: '03BB85',
+    };
+  });
+
+  return {
+    props: {
+      categories: formattedData,
+    },
+  };
+});
